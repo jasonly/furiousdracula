27ce4ab7b10eb8ea66e848f6fad8e0f44b093006
Jason Ly <jasonyx.ly@gmail.com>
2014-10-22 11:23:25 +1100

    Macka's latest FB upload

--- hunter.c
+++ hunter.c
@@ -8,27 +8,24 @@
 
 void decideHunterMove(HunterView gameState)
 {
-     if(giveMeTheRound(gameState) == 0){
-         if(whoAmI(gameState) == 0){
+    if(giveMeTheRound(gameState) == 0){
+        if(whoAmI(gameState) == 0){
             registerBestPlay("KL","G KL");
-         }
-          if(whoAmI(gameState) == 1){            
+        }
+        if(whoAmI(gameState) == 1){
             registerBestPlay("SR","Strasbourg"); // messages could be to long
-         }
-          if(whoAmI(gameState) == 2){
-              registerBestPlay("PA","Paris");
-         }
-          if(whoAmI(gameState) == 3){  
-             registerBestPlay("MU","Munich");      
-         }
-         
-     }else{
+        }
+        if(whoAmI(gameState) == 2){
+            registerBestPlay("PA","Paris");
+        }
+        if(whoAmI(gameState) == 3){
+            registerBestPlay("MU","Munich");
+        }
+        
+    }else{
         int *numLocations = NULL;
         int * locationArray;
         locationArray = whereCanIgo(gameState,numLocations,1, 1, 1);
-        registerBestPlay(idToAbbrev(locationArray[1]),"going");                                   
-     }
-    // TODO ...
-    // Replace the line below by something better
-    
+        registerBestPlay(idToAbbrev(locationArray[1]),"going");
+    }
 }
